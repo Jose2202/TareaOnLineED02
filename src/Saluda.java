39194978a429fef5f475ef51c134366aec5ce654
jose <jose@172.16.25.133>
2016-11-16 18:10:28 +0100

    Creamos la variable de clase _nombre

--- src/Saluda.java
+++ src/Saluda.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
 
 public class Saluda {
+	
+	
+	static // variable para almacenar el nombre
+	String nombre= "";
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hola, como te llamas: ");
+		// Metodo Scanner pedir nombre por teclado
+		
+		Scanner entrada = new Scanner (System.in);
+		
+		// Pedimos el nombre
+		
+		System.out.println("Ingresa tu nombre");
+		nombre = entrada.nextLine();
+		
+		
 
 	}
 
